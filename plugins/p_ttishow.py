6dc2fa89812128b4442262a06b784c3c214f06e3
HK-Productions <161631574+HK-Productions@users.noreply.github.com>
2024-09-24 20:08:51 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -41,7 +41,7 @@ async def save_group(bot, message):
                     InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                     InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                  ],[
-                    InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/KicchaFanMahi")
+                    InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="telegram.me/Darka143")
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
@@ -64,7 +64,7 @@ async def save_group(bot, message):
                                                                            InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                                                            InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                                                                         ],[
-                                                                           InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/KicchaFanMahi")
+                                                                           InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="telegram.me/Darka143")
                                                                          ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
@@ -89,8 +89,8 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('Support Group',url="https://t.me/+s9JRZ-rGDso4MTNl"),
-            InlineKeyboardButton('Owner', url="https://t.me/KicchaFanMaHi")
+            InlineKeyboardButton('Support Group',url="https://t.me/Thecinemacircle5"),
+            InlineKeyboardButton('Owner', url="telegram.me/Darka143")
         ],[
             InlineKeyboardButton('Use Me Here', url=f'https://t.me/{SUPPORT_CHAT}')
         ]]
