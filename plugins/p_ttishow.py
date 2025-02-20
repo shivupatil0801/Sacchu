2dd8cacb5087166971710ac3e61dbc915702d087
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2025-02-20 19:50:49 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -41,7 +41,7 @@ async def save_group(bot, message):
                     InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                     InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                  ],[
-                    InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="telegram.me/Darka143")
+                    InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="telegram.me/Harikushal")
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
@@ -64,7 +64,7 @@ async def save_group(bot, message):
                                                                            InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                                                            InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                                                                         ],[
-                                                                           InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="telegram.me/Darka143")
+                                                                           InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="telegram.me/Harikushal")
                                                                          ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
@@ -89,8 +89,8 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('Support Group',url="https://t.me/Thecinemacircle5"),
-            InlineKeyboardButton('Owner', url="telegram.me/Darka143")
+            InlineKeyboardButton('Support Group',url="https://t.me/+_Ss2e1bjdn5lZGRl"),
+            InlineKeyboardButton('Owner', url="telegram.me/Harikushal")
         ],[
             InlineKeyboardButton('Use Me Here', url=f'https://t.me/{SUPPORT_CHAT}')
         ]]
