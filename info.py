758650ec76172545766e8e58ddd797ce6cddad56
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2025-02-20 19:58:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://anikush8310:anikush8310@cluster0.bhrro.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Harikushal")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'HariKushal123')
 
