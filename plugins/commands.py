20729a4289f930c4bedc859ee5c3a3643250cf71
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2025-02-22 20:54:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -38,10 +38,19 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ᴛʜᴇ ɢʀᴏᴜᴘꜱ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('〄 Hᴇʟᴘ', callback_data='help'),
-                    InlineKeyboardButton('⍟ Aʙᴏᴜᴛ', callback_data='about')
+                    InlineKeyboardButton('ᴍᴏᴠɪᴇ ʀᴇǫᴜᴇꜱᴛ ɢʀᴏᴜᴘ 𝟷', url=f'https://t.me/+nDTaoJGRKJcxYmZl'),
+                    InlineKeyboardButton('ᴍᴏᴠɪᴇ ʀᴇǫᴜᴇꜱᴛ ɢʀᴏᴜᴘ 𝟸', url=f'https://t.me/HK_Movies_Request'),
+                ],[
+                    InlineKeyboardButton('ᴍᴏᴠɪᴇ ʀᴇǫᴜᴇꜱᴛ ɢʀᴏᴜᴘ 𝟹', url=f'https://t.me/Thecinemacircle3'),
+                    InlineKeyboardButton('ᴍᴏᴠɪᴇ ʀᴇǫᴜᴇꜱᴛ ɢʀᴏᴜᴘ 𝟺', url=f'https://t.me/Hari_Search')
+                ],[
+                    InlineKeyboardButton('ᴍᴏᴠɪᴇ ᴜᴘʟᴏᴀᴅ ᴄʜᴀɴɴᴇʟ', url=f'https://t.me/+63JiMC4lCjBjZDFl'),
+                    InlineKeyboardButton('ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ', url=f'https://t.me/TG_BOTS_UPDATE'),
+                ],[
+                    InlineKeyboardButton('™️ ᴘʀᴏᴍᴏᴛɪᴏɴ ᴄʜᴀɴɴᴇʟ', url=f'https://t.me/+w08SirY7LFphODQ9'),
+                ]]   
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         m=await message.reply_sticker("CAACAgUAAxkBAAI8UGTwJC8V4Y-GPFBhIjhnfLN8GZaOAAINAgACCTj5VdQIMJ_TbRMLMAQ") 
@@ -84,11 +93,19 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-                    InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ᴛʜᴇ ɢʀᴏᴜᴘꜱ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('〄 Hᴇʟᴘ', callback_data='help'),
-                    InlineKeyboardButton('⍟ Aʙᴏᴜᴛ', callback_data='about')
-                  ]]
+                    InlineKeyboardButton('ᴍᴏᴠɪᴇ ʀᴇǫᴜᴇꜱᴛ ɢʀᴏᴜᴘ 𝟷', url=f'https://t.me/+nDTaoJGRKJcxYmZl'),
+                    InlineKeyboardButton('ᴍᴏᴠɪᴇ ʀᴇǫᴜᴇꜱᴛ ɢʀᴏᴜᴘ 𝟸', url=f'https://t.me/HK_Movies_Request'),
+                ],[
+                    InlineKeyboardButton('ᴍᴏᴠɪᴇ ʀᴇǫᴜᴇꜱᴛ ɢʀᴏᴜᴘ 𝟹', url=f'https://t.me/Thecinemacircle3'),
+                    InlineKeyboardButton('ᴍᴏᴠɪᴇ ʀᴇǫᴜᴇꜱᴛ ɢʀᴏᴜᴘ 𝟺', url=f'https://t.me/Hari_Search')
+                ],[
+                    InlineKeyboardButton('ᴍᴏᴠɪᴇ ᴜᴘʟᴏᴀᴅ ᴄʜᴀɴɴᴇʟ', url=f'https://t.me/+63JiMC4lCjBjZDFl'),
+                    InlineKeyboardButton('ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ', url=f'https://t.me/TG_BOTS_UPDATE'),
+                ],[
+                    InlineKeyboardButton('™️ ᴘʀᴏᴍᴏᴛɪᴏɴ ᴄʜᴀɴɴᴇʟ', url=f'https://t.me/+w08SirY7LFphODQ9'),
+                ]]   
         reply_markup = InlineKeyboardMarkup(buttons)      
         await message.reply_photo(
             photo=random.choice(PICS),
